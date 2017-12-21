8110a12880852eaacb1fbdc2ba063ee651cca3b8
horiguchi yamato <ymt722@gmail.com>
2017-12-21 12:42:17 +0000

    new-column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130081401) do
+ActiveRecord::Schema.define(version: 20171221120330) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171130081401) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.integer "date"
   end
 
 end
